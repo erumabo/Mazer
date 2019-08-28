3d976853d4599a63639908db2f1eb096d89cd67e
erumabo <mabo.daniel@gmail.com>
2019-08-28 09:58:27 -0600

    bootstrap integrated

--- script/index.js
+++ script/index.js
@@ -1,2 +1,39 @@
-var mmaze = new Maze(10,10);
+var mmaze = new Maze(20,20);
 console.log(mmaze.height);
+mmaze.print();
+
+function tableSet(){
+  var tabla=document.getElementById("matriz");
+  var tr,td;
+  if(tabla){
+    for(i=0;i<mmaze.height;++i){
+      tr=tabla.insertRow();
+      for(j=0;j<mmaze.width;++j){
+        td=tr.insertCell();
+        console.log(mmaze.cellAt(i,j))
+        td.innerHTML=
+        '<div class="cell" style="background-color:'
+        +(mmaze.cellAt(i,j)===0?"black":"white")
+        +'"></div>';
+      }
+    }
+  }
+}
+
+function tableFill(){
+  mmaze.clear(0);
+  mmaze.genMaze(0,0);
+  var tabla=document.getElementById("matriz");
+  var tr,td;
+  if(tabla){
+    for(i=0;i<mmaze.height;++i){
+      for(j=0;j<mmaze.width;++j){
+        td=tabla.rows[i].cells[j];
+        td.innerHTML=
+          '<div class="cell" style="background-color:'
+         +(mmaze.cellAt(i,j)===0?"black":"white")
+         +'"></div>';
+      }
+    }
+  }
+}
--- script/maze.js
+++ script/maze.js
@@ -3,6 +3,8 @@ class Maze{
     this.width = W;
     this.height = H;
     this.maze = [];
+    this.clear(0);
+    this.genMaze(0,0);
   }
 
   cellAt(x,y){
@@ -11,7 +13,69 @@ class Maze{
         return this.maze[x][y]
       }
     }
-    return NaN;
+    return 0;
   }
   
+  print(){
+    for(var i=0;i<this.width;++i){
+      var s="";
+      for(var j=0;j<this.height;++j){
+        s=s+(this.maze[i][j]>0?'#':'[]')
+      }
+      console.log(s+"\n");
+    }
+  }
+  
+  genMaze(x, y){
+    this.maze[x][y]=1;
+    var dir=randomDir();
+    for(var i=0;i<4;++i){
+      var w=x,z=y;
+      switch(dir%10){
+        case 0:z--;break;
+        case 1:w++;break;
+        case 2:z++;break;
+        case 3:w--;break;
+      }
+      dir=Math.floor(dir/10);
+      if(bound(w,z,this.width,this.height) &&this.maze[w][z]===0&& open(this.maze,w,z))
+        this.genMaze(w,z);
+    }
+  }
+  
+  clear(v){
+    for(var i=0;i<this.width;++i){
+      this.maze[i]=[];
+      for(var j=0;j<this.height;++j){
+        this.maze[i][j]=v;
+      }
+    }
+  }
 }
+
+function randomDir(){
+  l=[1,2,3,0];
+  var t=0,i=0;
+  for(j=3;j>0;--j){
+    i=Math.floor(Math.random()*(j+1));
+    t=l[i];l[i]=l[j];l[j]=t;
+  }
+  t=0;
+  for(j=0;j<4;++j)t=t*10+l[j];
+  return t;
+}
+
+function bound(x,y,w,h){
+  return ((x>=0)&&(x<w)&&(y>=0)&&(y<h));
+}
+
+function open(maze,x,y){
+  var r=0;
+  if(maze[x+1]&&maze[x+1][y])r+=(maze[x+1][y]);
+  if(maze[x-1]&&maze[x-1][y])r+=(maze[x-1][y]);
+  if(maze[x]&&maze[x][y+1])r+=(maze[x][y+1]);
+  if(maze[x]&&maze[x][y-1])r+=(maze[x][y-1]);
+  if(r<=1) return true;
+  return false;
+}
+
